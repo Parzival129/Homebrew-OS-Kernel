6a579508d2cde70cb0953243b49e11fa41d69aa0
Parzival129 <beacheshiro@gmail.com>
2026-02-23 01:51:05 -0500

    changed the internal name of the kernel from myos to saru kernel, because I like monkeys and japanese.

--- iso.sh
+++ iso.sh
@@ -6,10 +6,10 @@ mkdir -p isodir
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
 
-cp sysroot/boot/myos.kernel isodir/boot/myos.kernel
+cp sysroot/boot/SaruKernel.kernel isodir/boot/SaruKernel.kernel
 cat > isodir/boot/grub/grub.cfg << EOF
-menuentry "myos" {
-	multiboot /boot/myos.kernel
+menuentry "SaruKernel" {
+	multiboot /boot/SaruKernel.kernel
 }
 EOF
-grub-mkrescue -o myos.iso isodir
+grub-mkrescue -o SaruKernel.iso isodir
